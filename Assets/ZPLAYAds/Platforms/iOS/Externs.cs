c51ecaa69df56b7ea8fc9b54b5454470b490aac0
ShunZhi Tang <tangshunzhi@zplay.com>
2019-10-30 16:54:15 +0800

    Implement iOS banner client

--- Assets/ZPLAYAds/Platforms/iOS/Externs.cs
+++ Assets/ZPLAYAds/Platforms/iOS/Externs.cs
@@ -5,6 +5,20 @@ using System.Runtime.InteropServices;
 
 namespace ZPLAYAds.iOS
 {
+    internal enum AtmosplayBannerAdSize
+    {
+        /// iPhone and iPod Touch ad size. Typically 320x50.
+        kAtmosplayAdsBanner320x50 = 1 << 0,
+        /// Leaderboard size for the iPad. Typically 728x90.
+        kAtmosplayAdsBanner728x90 = 1 << 1,
+        /// An ad size that spans the full width of the application in portrait orientation. The height is
+        /// typically 50 pixels on an iPhone/iPod UI, and 90 pixels tall on an iPad UI.
+        kAtmosplayAdsSmartBannerPortrait = 1 << 3,
+        /// An ad size that spans the full width of the application in landscape orientation. The height is
+        /// typically 32 pixels on an iPhone/iPod UI, and 90 pixels tall on an iPad UI.
+        kAtmosplayAdsSmartBannerLandscape = 1 << 4
+     }
+     
     class Externs
     {
 
@@ -75,6 +89,33 @@ namespace ZPLAYAds.iOS
 
         [DllImport("__Internal")]
         internal static extern void ZPLADSetRewardVideoChannelId(IntPtr rewardVideo, string channelId);
+        #endregion
+        #region banner externs
+        // banner
+        [DllImport("__Internal")]
+        internal static extern IntPtr InitAtmosplayBannerAd(IntPtr bannerView, string adAppId, string adUnitId);
+        [DllImport("__Internal")]
+        internal static extern void RequestBannerAd(IntPtr bannerView);
+        [DllImport("__Internal")]
+        internal static extern void ShowBannerView(IntPtr bannerView);
+        [DllImport("__Internal")]
+        internal static extern void HideBannerView(IntPtr bannerView);
+        [DllImport("__Internal")]
+        internal static extern void DestroyBannerView(IntPtr bannerView);
+        [DllImport("__Internal")]
+        internal static extern void SetBannerAdSize(IntPtr bannerView, AtmosplayBannerAdSize bannerSize);
+        [DllImport("__Internal")]
+        internal static extern void SetBannerPosition(IntPtr bannerView, int position);
+        [DllImport("__Internal")]
+        internal static extern void SetBannerChannelID(IntPtr bannerView, string channelId);
+        [DllImport("__Internal")]
+        internal static extern void SetBannerCallbacks(
+           IntPtr bannerView,
+           BannerClient.AtmosplayBannerDidReceiveAdCallback adReceivedCallback,
+           BannerClient.AtmosplayBannerDidFailToReceiveAdWithErrorCallback adFailedCallback,
+           BannerClient.AtmosplayBannerDidClickCallback adClickedCallback);
+
+
         #endregion
     }
 }
