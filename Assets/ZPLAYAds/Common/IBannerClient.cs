4959c3df80f57444261921d2ebc70311a3abde5d
ShunZhi Tang <tangshunzhi@zplay.com>
2019-10-30 16:52:38 +0800

    Create banner client interface

---
+++ Assets/ZPLAYAds/Common/IBannerClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ZPLAYAds.Api;
+
+namespace ZPLAYAds.Common
+{
+    public interface IBannerClient
+    {
+        // Ad event fired when the banner ad has been received.
+        event EventHandler<EventArgs> OnAdLoaded;
+        // Ad event fired when the banner ad has failed to load.
+        event EventHandler<AdFailedEventArgs> OnAdFailedToLoad;
+        // Ad event fired when the banner ad is clicked
+        event EventHandler<EventArgs> OnAdClicked;
+
+        // Creates a banner view and adds it to the view hierarchy.
+        void CreateBannerView(string adAppId, string adUnitId, BannerViewOptions options);
+        // Requests a new ad for the banner view.
+        void LoadAd();
+
+        // Shows the banner view on the screen.
+        void ShowBannerView();
+
+        // Hides the banner view from the screen.
+        void HideBannerView();
+
+        // Destroys a banner view.
+        void DestroyBannerView();
+    }
+}
